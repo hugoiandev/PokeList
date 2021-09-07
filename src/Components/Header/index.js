3062fb1939ff72cf0fe1cf802775aca33779452d
Hugo Ian <ahugaao@gmail.com>
2021-09-07 17:08:43 -0300

    adicionado estilos com o styled components

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -12,9 +12,8 @@ const Header = () => {
     return (
         <>
             <Topo>
-                <div className='container' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative'}}>
-                    <input ref={elementInput} type='text' className='buscar' placeholder='Buscar Pokémon por nome/numero' style={{width: '600px', margin: '0 0 0 20px', padding: '10px', borderRadius: '20px',
-                    border: '2px solid #d6d6d6', position: 'relative', zIndex: '1', borderRight: 'none', flex: '1'}}>
+                <div className='container'>
+                    <input ref={elementInput} type='text' className='buscar' placeholder='Buscar Pokémon por nome/numero'>
                     </input>
                     <a onClick={(event) => {
                         event.preventDefault()
@@ -22,18 +21,19 @@ const Header = () => {
                         global.setDadosPoke([])
                         global.fetchApi(`https://pokeapi.co/api/v2/pokemon/${elementCurrent.value.toLowerCase()}`)
                         global.setStatusBtn(false)
-                    }} href='/' style={{position: 'relative', left: '-20px', zIndex: '0', background: 'tomato', padding: '5px 40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottomRightRadius: '20px', borderTopRightRadius: '20px'}}>
-                        <img style={{height: '30px,', width: '30px'}} src='pesquisar.svg' alt='pesquisar'></img>
+                        }} href='/'>
+                        <img src='pesquisar.svg' alt='pesquisar'></img>
                     </a>
                 </div>
                 <div className='busca-avancada container'>
                     <nav>
                         <ul>
-                            <li style={{listStyle: 'none', display: 'flex', alignItems: 'center'}}>
+                            <li>
                                 <a href='/' onClick={(event) => {
                                     event.preventDefault()
                                     global.setActiveBusca(!global.activeBusca)
-                                    }} style={{textDecoration: 'none', fontSize: '1.1em', color: 'tomato'}}>Pesquisa Avançada</a>
+                                    }}>Pesquisa Avançada
+                                </a>
                                 <img src='seta.svg' alt='seta' style={{height: '15px', width: '15px', marginLeft: '10px', transform: global.activeBusca ? 'rotate(-90deg)' : 'rotate(90deg)', transition: '.5s'}}></img>
                             </li>
                         </ul>
--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -2,7 +2,68 @@ import styled from "styled-components";
 
 
 const Topo = styled.header`
-    background: red;
+
+    background: #ffffff;
+
+    div:nth-child(1) {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+
+        input {
+            width: 600px;
+            margin: 0 0 0 20px;
+            padding: 10px;
+            border-radius: 20px;
+            border: 2px solid #d6d6d6;
+            position: relative;
+            z-index: 1;
+            border-right: none;
+            flex: 1;
+        }
+
+        a {
+            position: relative;
+            left: -20px;
+            z-index: 0;
+            background: tomato;
+            padding: 5px 40px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-bottom-right-radius: 20px;
+            border-top-right-radius: 20px;
+
+            img {
+                height: 30px;
+                width: 30px;
+            }
+        }
+    }
+
+    div:nth-child(2) {
+        nav {
+            ul {
+                li {
+                    list-style: none;
+                    display: flex;
+                    align-items: center;
+                    a {
+                        text-decoration: none;
+                        font-size: 1.1em;
+                        color: tomato;
+                    }
+                    img {
+                        height: 15px;
+                        width: 15px;
+                        margin-left: 10px;
+                        transform: {};
+                    }
+                }
+            }
+        }
+    }
 
 `
 
