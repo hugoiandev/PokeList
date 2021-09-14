71454596a5c66e922165dbf7cbd3a80f456348b4
Hugo Ian <ahugaao@gmail.com>
2021-09-14 02:39:22 -0300

    delete menu

--- src/Components/Menu/index.js
+++
@@ -1,8 +0,0 @@
-//import React from 'react'
-//import { GlobalContext } from './GlobalContext'
-
-const Menu = () => {
-    //const global = React.useContext(GlobalContext)
-}
-
-export default Menu
