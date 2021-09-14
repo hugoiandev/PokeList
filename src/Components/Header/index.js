22fdfd143fbfb34a5b7ce1752f6724c69facf3e2
Hugo Ian <ahugaao@gmail.com>
2021-09-14 02:59:42 -0300

    corrigindo bugs

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,21 +5,20 @@ import Topo from './styled'
 
 const Header = () => {
     const global = React.useContext(GlobalContext)
-    const elementInput = React.useRef(null)
-    const elementCurrent = elementInput.current && elementInput.current
+    const [elementCurrent, setElementCurrent] = React.useState(null)
 
 
     return (
         <>
             <Topo>
                 <div className='container'>
-                    <input ref={elementInput} type='text' className='buscar' placeholder='Buscar Pokémon por nome/numero'>
+                    <input onChange={(event) => setElementCurrent(event.target.value)} type='text' className='buscar' placeholder='Buscar Pokémon por nome/numero'>
                     </input>
                     <a onClick={(event) => {
                         event.preventDefault()
                         global.setStatusSelect()
                         global.setDadosPoke([])
-                        global.fetchApi(`https://pokeapi.co/api/v2/pokemon/${elementCurrent.value.toLowerCase()}`)
+                        global.fetchApi(`https://pokeapi.co/api/v2/pokemon/${elementCurrent.toLowerCase()}`)
                         global.setStatusBtn(false)
                         }} href='/'>
                         <img src='pesquisar.svg' alt='pesquisar'></img>
